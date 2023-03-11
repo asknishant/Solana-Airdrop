ff45da50d0af24d03a793ca9845ac2607de726b7
Nishant Kumar <asknishant.39@gmail.com>
2023-03-11 22:55:05 +0530

    solana dropped in one go

---
+++ src/Alert.js
@@ -0,0 +1,8 @@
+// Alert.js
+import React from 'react'
+
+function Alert() {
+  return <div className="alert">Please enter some text!</div>
+}
+
+export default Alert
--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { useState } from 'react'
+import './App.css'
+import Alert from './Alert'
+import { Connection, PublicKey } from '@solana/web3.js'
 
 function App() {
+  const [inputValue, setInputValue] = useState('')
+  const [showAlert, setShowAlert] = useState(false)
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value)
+  }
+  const handleButtonClick = () => {
+    if (inputValue.trim() === '') {
+      setShowAlert(true)
+      setTimeout(() => {
+        setShowAlert(false)
+      }, 3000)
+    } else {
+      alert(`You entered: ${inputValue}`)
+      setInputValue('')
+    }
+    // connection
+    const connection = new Connection('https://api.devnet.solana.com')
+    let myPublicKey = new PublicKey(inputValue)
+    const fn = async () => {
+      // 1e9 lamports = 10^9 lamports = 1 SOL
+      let txhash = connection.requestAirdrop(myPublicKey, 1e9)
+      console.log(`txhash: ${txhash}`)
+    }
+    fn()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Enter the address</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="Enter text here"
+      />
+      <button onClick={handleButtonClick}>Submit</button>
+      {showAlert && <Alert />}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
